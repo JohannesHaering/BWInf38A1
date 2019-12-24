ab4825a380fc1e41ea1975ae5413974a91ad7faf
Johannes Häring <johannes.haering99@gmx.de>
2019-12-24 13:59:57 +0100

    Added Network data structure

---
+++ Network.py
@@ -0,0 +1,15 @@
+class Network:
+    def __init__(this, vertices, edges)
+        this.vertices = vertices
+        this.edges = edges
+
+class Vertice:
+    def __init__(this, travelled, batteryCharge)
+        this.travelled = travelled
+        this.batteryCharge = batteryCharge
+
+class Edge:
+    def __init__(this, v1, v2, weight)
+        this.v1 = v1
+        this.v2 = v2
+        this.weight = weight
